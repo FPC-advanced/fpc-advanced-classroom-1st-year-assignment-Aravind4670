c5426cd96393e9d553589937bafd7af8446dad36
Aravind4670 <ntaravind.bsc23@rvu.edu.in>
2023-12-05 14:24:07 +0530

    committed 12/5/2023, 2:24:06 PM

--- set02/problem01.c
+++ set02/problem01.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
- 
-void input(float *base, float *height){
-    printf("enter height and base");
-    scanf("%f%f",base,height);
-}
-void find_area(float base , float height, float *area){
-    *area = 0.5 * base *height;
 
+void input(float *base, float *height) {
+    printf("Enter height and base: ");
+    scanf("%f %f", base, height);
 }
-void output(float base, float height, float area){
-    printf("%f*%f*0.5",area);
+
+void find_area(float base, float height, float *area) {
+    *area = 0.5 * base * height;
 }
-int main(){
-    float h,b,area;
-    input(&h,&b);
-    find_area(b,h,&area);
-    output(h,b,area);
+
+void output(float base, float height, float area) {
+    printf("Area of the triangle with base %.2f and height %.2f is %.2f\n", base, height, area);
+}
+
+int main() {
+    float h, b, area;
+    input(&h, &b);
+    find_area(b, h, &area);
+    output(h, b, area);
     return 0;
 }
