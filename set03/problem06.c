01395786ab55190dc989cb973071c602e6face40
Aravind4670 <ntaravind.bsc23@rvu.edu.in>
2023-12-05 14:28:07 +0530

    committed 12/5/2023, 2:28:06 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,24 +1,36 @@
-#include<stdio.h>
-void input_string(char* a, char* b)
-{
-    printf("enter the string1");
-    scanf("%s",a);
-    printf("enter the substring");
-    scanf("%s",b);
+#include <stdio.h>
+
+void input_string(char* a, char* b) {
+    printf("Enter the string: ");
+    scanf("%s", a);
+    printf("Enter the substring: ");
+    scanf("%s", b);
 }
-int sub_str_index(char* string, char* substring){
-int i,c = 0;
-for(i =0;string[i]!=*substring;i++);
-return i;
+
+int sub_str_index(char* string, char* substring) {
+    int i, j;
+    for (i = 0; string[i] != '\0'; i++) {
+        for (j = 0; substring[j] != '\0' && string[i + j] == substring[j]; j++);
+        if (substring[j] == '\0') {
+            return i;
+        }
+    }
+    return -1; // Return -1 if substring is not found
 }
-void output(int index){
-    printf("the index is %d",index);
+
+void output(int index) {
+    if (index != -1) {
+        printf("The substring starts at index %d\n", index);
+    } else {
+        printf("Substring not found in the given string\n");
+    }
 }
-int main(){
+
+int main() {
     int i;
-    char str[100],subs[100];
-     input_string(str,subs);
-   i =   subs_Str_index(str,subs);
-   output(i);
-   return 0;
+    char str[100], subs[100];
+    input_string(str, subs);
+    i = sub_str_index(str, subs);
+    output(i);
+    return 0;
 }
