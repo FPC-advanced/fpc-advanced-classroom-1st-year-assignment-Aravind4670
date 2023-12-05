1d1bba2a235985e058f67de19d64e8e4646d593e
Aravind4670 <ntaravind.bsc23@rvu.edu.in>
2023-12-05 14:09:23 +0530

    committed 12/5/2023, 2:09:22 PM

--- set01/problem08.c
+++ set01/problem08.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 void  inp(int n , int arr[n]);
-int sum(int arr[],int n);
+int sum(int n,int arr[n]);
 void  inp(int n , int arr[n]){
     printf("enter the range");
     scanf("%d",&n);
     printf("enter a element");
-    for(int i=0;i<=n;i++){
+    for(int i=0;i<n;i++){
 scanf("%d",&arr[i]);
     }
 }
 
-int sum(int arr[],int n){
-    int sum = 0;
-    for(int i=0;i<=n;i++){
-    sum += arr[i];
+int sum(int n,int arr[n]){
+    int Sum = 0;
+    for(int i=0;i<n;i++){
+    Sum += arr[i];
     }
-    return sum;
+    return Sum;
 }
 
 int main(){
     int n,arr[n];
     inp(n,arr);
-  int sume =   sum(arr,n);
+  int sume =   sum(n,arr);
 printf("%d",sume);
 return 0;
 }
