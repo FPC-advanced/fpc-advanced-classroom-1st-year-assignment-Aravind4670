339c2383ecf0aa1dae15c658f5e0796179e4cd94
Aravind4670 <ntaravind.bsc23@rvu.edu.in>
2023-12-05 14:23:37 +0530

    committed 12/5/2023, 2:23:36 PM

--- set01/problem12.c
+++ set01/problem12.c
@@ -1,50 +1,59 @@
+#include <stdio.h>
+
 struct _complex {
-	float r,im;
+    float r, im;
 };
 typedef struct _complex Complex;
 
-int get_n(){int n;
-     printf("enter no of no");
-     scanf("%d",&n);
-     return n;
+int get_n() {
+    int n;
+    printf("Enter the number of complex numbers: ");
+    scanf("%d", &n);
+    return n;
 }
-Complex input_complex(){
-    Complex a;
-       printf("enter a real");
-    scanf("%f",&a.r);
-    printf("enter a imag");
-    scanf("%f",&a.im);
-return a;
-}
-void input_n_complex(int n, Complex c[n]){
-    int i=0;
-    for(i=0;i<n;i++){
-        printf("enter the complex no \n\n");
-     c[i] = input_complex();
-    }
 
-}
-Complex add(Complex a, Complex b){
-        Complex sum;
-    sum.r = a.r+b.r;
-    sum.im = a.im+b.im;
-    return sum;
-}
-Complex add_n_complex(int n, Complex c[n]){
-    Complex a={0,0};
-    int i;
-    for(i=0;i<n;i++){
-       add(a,c[i]);
-    }
+Complex input_complex() {
+    Complex a;
+    printf("Enter the real part: ");
+    scanf("%f", &a.r);
+    printf("Enter the imaginary part: ");
+    scanf("%f", &a.im);
     return a;
 }
-void output(Complex result){
-printf("%f + %fi",result.r,result.im);
+
+void input_n_complex(int n, Complex c[n]) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("Enter complex number %d:\n", i + 1);
+        c[i] = input_complex();
+    }
 }
-int main(){
-    int n = get_n();Complex a[n];
-    input_n_complex(n,a);
-   Complex b =  add_n_complex(n,a);
+
+Complex add(Complex a, Complex b) {
+    Complex sum;
+    sum.r = a.r + b.r;
+    sum.im = a.im + b.im;
+    return sum;
+}
+
+Complex add_n_complex(int n, Complex c[n]) {
+    Complex result = {0, 0};
+    int i;
+    for (i = 0; i < n; i++) {
+        result = add(result, c[i]);
+    }
+    return result;
+}
+
+void output(Complex result) {
+    printf("Sum: %f + %fi\n", result.r, result.im);
+}
+
+int main() {
+    int n = get_n();
+    Complex a[n];
+    input_n_complex(n, a);
+    Complex b = add_n_complex(n, a);
     output(b);
     return 0;
 }
